30e7f1a17a279d476bea7be81a31504b450286fc
Alex Carter <acarter010204@gmail.com>
2021-04-08 23:45:15 +1000

    Added initial work on graphql server

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # CSCI334Project
 CSCI334 COVID Project
+
+
+## Server
+```pip install "graphene>=2.0"```
+```pip install sqlalchemy```
+```pip install graphene_sqlalchemy```
+```pip install flask_graphql```
---
+++ server/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base, engine
+
+class PersonModel(Base):
+    __tablename__ = 'person'
+    uuid = Column(Integer, primary_key=True)
+    
+class ArticleModel(Base):
+    __tablename__ = 'article'
+    uuid = Column(Integer, primary_key=True)
+    person_id = Column(ForeignKey("person.uuid"))
+    
+Base.prepare(engine)
