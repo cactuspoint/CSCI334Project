db219fc8f707793c0318877e2881902a754c8bda
Alex Carter <acarter010204@gmail.com>
2021-04-10 20:30:00 +1000

    Added mutation style auth

--- server/database.py
+++ server/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base, DeferredReflection
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 engine = create_engine('sqlite:////home/iommu/Desktop/CSCI334Project/server/test.db', echo=True)
-db_session = scoped_session(sessionmaker(bind=engine))
+session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 Base = declarative_base(cls=DeferredReflection)
-Base.query = db_session.query_property()
+Base.query = session.query_property()
--- server/server.py
+++ server/server.py
@@ -2,18 +2,25 @@
 
 from flask import Flask
 
-from database import db_session
+from database import session
 from flask_graphql import GraphQLView
 from schema import schema
+from flask_graphql_auth import GraphQLAuth
 
 app = Flask(__name__)
+
+app.config["JWT_SECRET_KEY"] = "secure"
+app.config["REFRESH_EXP_LENGTH"] = 30
+app.config["ACCESS_EXP_LENGTH"] = 10
 app.debug = True
 
-app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True, context={'session': db_session}))
+auth = GraphQLAuth(app)
+
+app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True, context={'session': session}))
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
-    db_session.remove()
+    session.remove()
 
 if __name__ == '__main__':
     app.run()
