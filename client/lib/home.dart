de1dfe16686be09bb174c6260e96e46845cf322a
Alex Carter <acarter010204@gmail.com>
2021-05-15 22:54:13 +1000

    added query to phone page

--- client/lib/home.dart
+++ client/lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'globals.dart' as globals;
 
 class HomePage extends StatefulWidget {
@@ -9,18 +10,47 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    const String getPerson = '''
+    query GetPerson(\$uuid: String!, \$jwt: String!) {
+      person(uuid: \$uuid, jwt: \$jwt) {
+        uuid,
+        phoneNum,
+        firstName, 
+        lastName
+      }
+    }
+    ''';
     return Scaffold(
-        // appBar: AppBar(
-        //   title: Text('Analytics'),
-        // ),
-        body: Center(
-            // child: Text('This is the Home view.'),
-            child: TextButton(
-      onPressed: () {
-        Navigator.pop(context);
-        Navigator.pop(context);
-      },
-      child: Text(globals.jwt),
-    )));
+      body: Center(
+          child: Query(
+        options: QueryOptions(
+          document: gql(getPerson), // this is the query string you just created
+          variables: {'uuid': "", 'jwt': globals.jwt},
+          pollInterval: Duration(seconds: 10),
+        ),
+        builder: (QueryResult result,
+            {VoidCallback refetch, FetchMore fetchMore}) {
+          if (result.hasException) {
+            return Text(result.exception.toString());
+          }
+
+          if (result.isLoading) {
+            return Text('Loading');
+          }
+
+          String firstName = result.data['person']['firstName'];
+          String lastName = result.data['person']['lastName'];
+
+          return ListView.builder(
+            itemCount: 1,
+            itemBuilder: (context, index) {
+              // final repository = repositories[index];
+
+              return Text("Hello " + firstName + " " + lastName);
+            },
+          );
+        },
+      )),
+    );
   }
 }
