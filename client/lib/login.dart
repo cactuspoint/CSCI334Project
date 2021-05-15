430a370eb9d1d74aba2f5f95370d1bcf4c7183c0
Gabriel Komarnicki <20436870+gabkomarnicki@users.noreply.github.com>
2021-05-15 13:22:51 +1000

    Added a SingleChildScrollView to login.dart

--- client/lib/login.dart
+++ client/lib/login.dart
@@ -13,97 +13,95 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       // appBar: AppBar(title: Text('Alerts'),),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.fromLTRB(16, 24, 8, 64),
-            // padding: EdgeInsets.only(left: 8),
-            // child: Text('Log in'),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  'Have an account?',
-                  style: TextStyle(fontSize: 32),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(16, 16, 16, 8),
-            alignment: Alignment.center,
-            child: TextField(
-              keyboardType: TextInputType.phone,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Mobile number',
-                prefixText: '04',
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.fromLTRB(16, 200, 8, 64),
+              // padding: EdgeInsets.only(left: 8),
+              // child: Text('Log in'),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Have an account?',
+                    style: TextStyle(fontSize: 32),
+                  ),
+                ],
               ),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(16, 4, 16, 16),
-            alignment: Alignment.center,
-            child: TextField(
-              keyboardType: TextInputType.visiblePassword,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
+            Container(
+              padding: EdgeInsets.fromLTRB(16, 16, 16, 8),
+              alignment: Alignment.center,
+              child: TextField(
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Mobile number',
+                  prefixText: '04',
+                ),
               ),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(16, 120, 16, 4),
-            // padding: EdgeInsets.symmetric(horizontal: 8.0),
-            child: SizedBox(
-              width: double.infinity,
-              child: TextButton(
-                style: TextButton.styleFrom(
-                  padding: EdgeInsets.symmetric(vertical: 16.0),
+            Container(
+              padding: EdgeInsets.fromLTRB(16, 4, 16, 16),
+              alignment: Alignment.center,
+              child: TextField(
+                keyboardType: TextInputType.visiblePassword,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
                 ),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text(
-                  'CANCEL',
-                  style: TextStyle(
-                    fontSize: 16,
-                    // color: Colors.white,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(16, 190, 16, 4),
+              // padding: EdgeInsets.symmetric(horizontal: 8.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.symmetric(vertical: 16.0),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text(
+                    'CANCEL',
+                    style: TextStyle(
+                      fontSize: 16,
+                      // color: Colors.white,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(16, 4, 16, 24),
-            // padding: EdgeInsets.symmetric(horizontal: 8.0),
-            child: SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.symmetric(vertical: 16.0),
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => DashboardPage()),
-                  );
-                },
-                child: Text(
-                  'LOG IN',
-                  style: TextStyle(fontSize: 16),
+            Container(
+              padding: EdgeInsets.fromLTRB(16, 4, 16, 24),
+              // padding: EdgeInsets.symmetric(horizontal: 8.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(vertical: 16.0),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => DashboardPage()),
+                    );
+                  },
+                  child: Text(
+                    'LOG IN',
+                    style: TextStyle(fontSize: 16),
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () {},
-
-      // ),
     );
   }
 }
