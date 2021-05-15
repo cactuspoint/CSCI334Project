bc194de9c96517ae4f2063241f78e777049a6e7a
Gabriel Komarnicki <20436870+gabkomarnicki@users.noreply.github.com>
2021-05-15 13:18:26 +1000

    Added keyboardType on the sign up textfields

--- client/lib/signup.dart
+++ client/lib/signup.dart
@@ -35,6 +35,7 @@ class _SignUpState extends State<SignUpPage> {
             padding: EdgeInsets.fromLTRB(16, 16, 16, 8),
             alignment: Alignment.center,
             child: TextField(
+              keyboardType: TextInputType.phone,
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: 'Mobile number',
@@ -46,6 +47,7 @@ class _SignUpState extends State<SignUpPage> {
             padding: EdgeInsets.fromLTRB(16, 4, 16, 8),
             alignment: Alignment.center,
             child: TextField(
+              keyboardType: TextInputType.visiblePassword,
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: 'Password',
@@ -56,6 +58,7 @@ class _SignUpState extends State<SignUpPage> {
             padding: EdgeInsets.fromLTRB(16, 4, 16, 16),
             alignment: Alignment.center,
             child: TextField(
+              keyboardType: TextInputType.visiblePassword,
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: 'Confirm Password',
