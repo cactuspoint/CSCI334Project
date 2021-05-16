eaa19907c3319446e2cffb136ac2e273b28622c2
Alex Carter <acarter010204@gmail.com>
2021-05-16 12:39:43 +1000

    Added working signup

--- client/lib/signup.dart
+++ client/lib/signup.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'dashboard.dart';
+import 'globals.dart' as globals;
 
 class SignUpPage extends StatefulWidget {
   @override
@@ -8,105 +10,210 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUpPage> {
+  OnMutationUpdate get update => (cache, result) {
+        bool showDash = true;
+        if (result.hasException) {
+          print(result.exception);
+          if (result.exception.graphqlErrors[0] != null) {
+            if (result.exception.graphqlErrors[0].message ==
+                "error: user already exists") {
+              passController.text = "";
+              showDash = false;
+              Navigator.pop(context);
+              _simpleAlert(
+                  context, "Error user already exists with that phone number");
+            }
+          }
+          if (showDash) {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => DashboardPage()));
+          }
+        } else {
+          globals.jwt = result.data['action']['accessToken'].toString();
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => DashboardPage()));
+        }
+      };
+
+  final phoneController = TextEditingController();
+  final passController = TextEditingController();
+  final passControllerBk = TextEditingController();
+  final fNController = TextEditingController();
+  final lNController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    const String signup = '''
+    mutation SignUp(\$phoneNum: String!, \$password: String!, \$firstName: String!, \$lastName: String!) {
+      action: signUp(phoneNum: \$phoneNum, password: \$password, firstName: \$firstName, lastName: \$lastName){
+        accessToken
+      }
+    }
+    ''';
+
     return Scaffold(
       // resizeToAvoidBottomInset: false,
       body: SingleChildScrollView(
         child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.fromLTRB(16, 160, 8, 64),
-            // padding: EdgeInsets.only(left: 8),
-            // child: Text('Log in'),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  'Create an account',
-                  style: TextStyle(fontSize: 32),
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.fromLTRB(16, 20, 8, 30),
+              // padding: EdgeInsets.only(left: 8),
+              // child: Text('Log in'),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Create an account',
+                    style: TextStyle(fontSize: 32),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(16, 1, 16, 8),
+              alignment: Alignment.center,
+              child: TextField(
+                keyboardType: TextInputType.phone,
+                controller: phoneController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Mobile number',
                 ),
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(16, 16, 16, 8),
-            alignment: Alignment.center,
-            child: TextField(
-              keyboardType: TextInputType.phone,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Mobile number',
-                prefixText: '04',
               ),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(16, 4, 16, 8),
-            alignment: Alignment.center,
-            child: TextField(
-              keyboardType: TextInputType.visiblePassword,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
-              ),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(16, 4, 16, 16),
-            alignment: Alignment.center,
-            child: TextField(
-              keyboardType: TextInputType.visiblePassword,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Confirm Password',
-              ),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(16, 160, 16, 4),
-            // padding: EdgeInsets.symmetric(horizontal: 8.0),
-            child: SizedBox(
-              width: double.infinity,
-              child: TextButton(
-                style: TextButton.styleFrom(
-                  padding: EdgeInsets.symmetric(vertical: 16.0),
+            Container(
+              padding: EdgeInsets.fromLTRB(16, 4, 16, 8),
+              alignment: Alignment.center,
+              child: TextField(
+                keyboardType: TextInputType.visiblePassword,
+                controller: passController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
                 ),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text(
-                  'CANCEL',
-                  style: TextStyle(
-                    fontSize: 16,
-                    // color: Colors.white,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(16, 4, 16, 16),
+              alignment: Alignment.center,
+              child: TextField(
+                keyboardType: TextInputType.visiblePassword,
+                controller: passControllerBk,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Confirm Password',
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(16, 4, 16, 16),
+              alignment: Alignment.center,
+              child: TextField(
+                controller: fNController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'First Name',
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(16, 4, 16, 16),
+              alignment: Alignment.center,
+              child: TextField(
+                controller: lNController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Last Name',
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(16, 20, 16, 4),
+              // padding: EdgeInsets.symmetric(horizontal: 8.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.symmetric(vertical: 16.0),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text(
+                    'CANCEL',
+                    style: TextStyle(
+                      fontSize: 16,
+                      // color: Colors.white,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(16, 4, 16, 24),
-            // padding: EdgeInsets.symmetric(horizontal: 8.0),
-            child: SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.symmetric(vertical: 16.0),
-                ),
-                onPressed: () {},
-                child: Text(
-                  'SIGN UP',
-                  style: TextStyle(fontSize: 16),
-                ),
+            Container(
+              padding: EdgeInsets.fromLTRB(16, 0, 16, 0),
+              // padding: EdgeInsets.symmetric(horizontal: 8.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: Mutation(
+                    options: MutationOptions(
+                      document: gql(signup),
+                      update: update,
+                      // onError: (OperationException error) =>
+                      //     _simpleAlert(context, error.toString()),
+                    ),
+                    builder: (RunMutation _authenticate, QueryResult result) {
+                      return SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.symmetric(vertical: 16.0),
+                          ),
+                          onPressed: () {
+                            if (passController.text == passControllerBk.text) {
+                              _authenticate({
+                                'phoneNum': phoneController.text,
+                                'password': passController.text,
+                                'firstName': fNController.text,
+                                'lastName': lNController.text,
+                              });
+                            } else {
+                              passController.text = "";
+                              passControllerBk.text = "";
+                              _simpleAlert(context, "Passwords don't match");
+                            }
+                          },
+                          child: Text(
+                            'SIGNUP',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                      );
+                    }),
               ),
             ),
-          ),
-        ],
-      ),
+          ],
+        ),
       ),
     );
   }
 }
+
+void _simpleAlert(BuildContext context, String text) => showDialog<AlertDialog>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(text),
+          actions: <Widget>[
+            SimpleDialogOption(
+              child: const Text('DISMISS'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      },
+    );
