050fceb22c2f9b2b854606912b841e7da09b67b2
cactuspoint <pollywaffle101@gmail.com>
2021-05-17 12:59:09 +1000

    add Visit to local database

--- client/lib/utils/helpers/database-helper.dart
+++ client/lib/utils/helpers/database-helper.dart
@@ -1,4 +1,28 @@
-class DatabaseHelper {}
+import 'dart:async';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static Future<Database> getDatabase() async {
+    return openDatabase(
+      join(await getDatabasesPath(), 'visits_database.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          "CREATE TABLE visits(id INTEGER PRIMARY KEY, datetime TEXT, location TEXT)",
+        );
+      },
+    );
+  }
+
+  static Future<void> insertVisit(Visit visit) async {
+    final Database db = await getDatabase();
+    await db.insert(
+      'visits',
+      visit.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+}
 
 class Visit {
   final int id; // dummy id for primary key
