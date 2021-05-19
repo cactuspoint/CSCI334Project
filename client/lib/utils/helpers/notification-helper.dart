423a55a59011e01711997b97d59cb4a7d1c2f17c
cactuspoint <pollywaffle101@gmail.com>
2021-05-20 09:31:14 +1000

    add notification helper skeleton code for #11

---
+++ client/lib/utils/helpers/notification-helper.dart
@@ -0,0 +1,7 @@
+/// A helper class to generate a native notification.
+/// ```dart
+/// NotificationHelper.generateNotification("Example Title", "Example body");
+/// ```
+class NotificationHelper {
+  static Future<void> generateNotification(String title, String body) async {}
+}
