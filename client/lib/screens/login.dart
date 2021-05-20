5d09c5a22893cfd1af16748bdf67ce11ee2eeb56
cactuspoint <pollywaffle101@gmail.com>
2021-05-20 19:16:07 +1000

    refactor screen login

--- client/lib/screens/login.dart
+++ client/lib/screens/login.dart
@@ -50,26 +50,17 @@ class _LoginPageState extends State<LoginPage> {
       }
     }
     ''';
-
     return Scaffold(
-      // appBar: AppBar(
-      //     title: Text('COVID Alert'),
-      //   ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
             padding: EdgeInsets.fromLTRB(16, 24, 8, 64),
-            // padding: EdgeInsets.only(left: 8),
-            // child: Text('Log in'),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Text(
-                  'Have an account?',
-                  style: TextStyle(fontSize: 32),
-                ),
+                Text('Have an account?', style: TextStyle(fontSize: 32)),
               ],
             ),
           ),
@@ -80,9 +71,7 @@ class _LoginPageState extends State<LoginPage> {
               keyboardType: TextInputType.phone,
               controller: phoneController,
               decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Mobile number',
-              ),
+                  border: OutlineInputBorder(), labelText: 'Mobile number'),
             ),
           ),
           Container(
@@ -98,35 +87,24 @@ class _LoginPageState extends State<LoginPage> {
           ),
           Container(
             padding: EdgeInsets.fromLTRB(16, 4, 16, 4),
-            // padding: EdgeInsets.symmetric(horizontal: 8.0),
             child: SizedBox(
               width: double.infinity,
               child: TextButton(
                 style: TextButton.styleFrom(
-                  padding: EdgeInsets.symmetric(vertical: 16.0),
-                ),
+                    padding: EdgeInsets.symmetric(vertical: 16.0)),
                 onPressed: () {
                   Navigator.pop(context);
                 },
-                child: Text(
-                  'CANCEL',
-                  style: TextStyle(
-                    fontSize: 16,
-                    // color: Colors.white,
-                  ),
-                ),
+                child: Text('CANCEL', style: TextStyle(fontSize: 16)),
               ),
             ),
           ),
           Container(
             padding: EdgeInsets.fromLTRB(16, 4, 16, 24),
-            // padding: EdgeInsets.symmetric(horizontal: 8.0),
             child: Mutation(
                 options: MutationOptions(
                   document: gql(authenticate),
                   update: update,
-                  // onError: (OperationException error) =>
-                  //     _simpleAlert(context, error.toString()),
                 ),
                 builder: (RunMutation _authenticate, QueryResult result) {
                   return SizedBox(
@@ -141,10 +119,7 @@ class _LoginPageState extends State<LoginPage> {
                           'password': passController.text
                         });
                       },
-                      child: Text(
-                        'LOG IN',
-                        style: TextStyle(fontSize: 16),
-                      ),
+                      child: Text('LOG IN', style: TextStyle(fontSize: 16)),
                     ),
                   );
                 }),
@@ -163,9 +138,7 @@ void _simpleAlert(BuildContext context, String text) => showDialog<AlertDialog>(
           actions: <Widget>[
             SimpleDialogOption(
               child: const Text('DISMISS'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
+              onPressed: () => Navigator.of(context).pop(),
             )
           ],
         );
