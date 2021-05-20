a9d2287e24004874971a3652e47d554cc3075fb6
Alex Carter <acarter010204@gmail.com>
2021-05-20 16:22:55 +1000

    updated model to include "infected" bool

--- server/gql_server/models.py
+++ server/gql_server/models.py
@@ -1,8 +1,8 @@
-from sqlalchemy import *
+import sqlalchemy as sqa
 from sqlalchemy.orm import scoped_session, sessionmaker, relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///db.sqlite3', convert_unicode=True)
+engine = sqa.create_engine('sqlite:///db.sqlite3', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 Base = declarative_base()
@@ -10,11 +10,12 @@ Base.query = db_session.query_property()
 
 class Person(Base):
     __tablename__ = 'Person'
-    uuid = Column(String, primary_key=True)
-    phone_num = Column(String)
-    password = Column(String)
-    first_name = Column(String)
-    last_name = Column(String)
-    vaccine_name = Column(String)
-    vaccine_inj = Column(Integer)
-    vaccine_rec_inj = Column(Integer)
+    uuid = sqa.Column(sqa.String, primary_key=True)
+    phone_num = sqa.Column(sqa.String)
+    password = sqa.Column(sqa.String)
+    first_name = sqa.Column(sqa.String)
+    last_name = sqa.Column(sqa.String)
+    infected = sqa.Column(sqa.Boolean)
+    vaccine_name = sqa.Column(sqa.String)
+    vaccine_inj = sqa.Column(sqa.Integer)
+    vaccine_rec_inj = sqa.Column(sqa.Integer)
