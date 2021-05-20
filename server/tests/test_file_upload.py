0f0dc2e5c2b89e679ec59b834310af02524c028f
Alex Carter <acarter010204@gmail.com>
2021-05-20 16:23:20 +1000

    fixed testing code

--- server/tests/test_file_upload.py
+++ server/tests/test_file_upload.py
@@ -5,6 +5,6 @@ def test_version():
     assert __version__ == '0.1.0'
 
 def test_upload_picture():
-with open("./assets/face.png", "rb") as a_file:
-    file_dict = {"file": a_file}
-    response = requests.post("http://127.0.0.1:5000/upload", files=file_dict)
+    with open("./assets/face.png", "rb") as a_file:
+        file_dict = {"file": a_file}
+        response = requests.post("http://127.0.0.1:5000/upload", files=file_dict)
