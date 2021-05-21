230a38f0b2f6de5a8a70fd4a5ff8a0992479a699
cactuspoint <pollywaffle101@gmail.com>
2021-05-21 10:18:23 +1000

    create library for alerts functionality

--- client/lib/utils/constants/app_globals.dart
+++ client/lib/utils/constants/app_globals.dart
@@ -1,5 +1,8 @@
 library client.globals;
 
+import 'package:client/utils/helpers/alerts-helper.dart';
+
 String jwt = "";
 String currentLocation = ""; // iso6709
 bool dbExposedAttached = false;
+List<ListItem> alerts;
