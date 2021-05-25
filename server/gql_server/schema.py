8b17bc53bfdd953eb021b0f9f0641ff1fcce21c6
Alex Carter <acarter010204@gmail.com>
2021-05-25 17:11:32 +1000

    updated formatting

--- server/gql_server/schema.py
+++ server/gql_server/schema.py
@@ -7,8 +7,8 @@ from graphene_sqlalchemy import SQLAlchemyConnectionField, SQLAlchemyObjectType
 from sqlalchemy import and_
 from flask import request
 import pathlib
-from . middleware import encrypt_jwt, decrypt_jwt
-from . models import db_session, Person as PersonModel
+from .middleware import encrypt_jwt, decrypt_jwt
+from .models import db_session, Person as PersonModel
 from . import UPLOAD_DIR
 
 # Setup Models
@@ -17,8 +17,9 @@ from . import UPLOAD_DIR
 class Person(SQLAlchemyObjectType):
     class Meta:
         model = PersonModel
-        exclude = ('password',)
-        interfaces = (graphene.relay.Node, )
+        exclude = ("password",)
+        interfaces = (graphene.relay.Node,)
+
 
 # Mutations
 
@@ -34,18 +35,26 @@ class SignUpMutation(graphene.Mutation):
 
     @classmethod
     def mutate(cls, _, info, phoneNum, password, firstName, lastName):
-        query = db_session.query(PersonModel).filter(
-            PersonModel.phone_num == phoneNum).first()
+        query = (
+            db_session.query(PersonModel)
+            .filter(PersonModel.phone_num == phoneNum)
+            .first()
+        )
         if not query:
-            user = PersonModel(uuid=str(uuid.uuid1()), phone_num=phoneNum,
-                               password=password, first_name=firstName, last_name=lastName)
+            user = PersonModel(
+                uuid=str(uuid.uuid1()),
+                phone_num=phoneNum,
+                password=password,
+                first_name=firstName,
+                last_name=lastName,
+            )
             db_session.add(user)
             db_session.commit()
             return SignUpMutation(
                 access_token=encrypt_jwt(user.uuid),
             )
         else:
-            raise GraphQLError('error: user already exists')
+            raise GraphQLError("error: user already exists")
 
 
 class AuthMutation(graphene.Mutation):
@@ -57,30 +66,38 @@ class AuthMutation(graphene.Mutation):
 
     @classmethod
     def mutate(cls, _, info, phoneNum, password):
-        query = db_session.query(PersonModel).filter(
-            PersonModel.phone_num == phoneNum).first()
+        query = (
+            db_session.query(PersonModel)
+            .filter(PersonModel.phone_num == phoneNum)
+            .first()
+        )
         if query:
             if query.password == password:
                 return AuthMutation(
                     access_token=encrypt_jwt(query.uuid),
                 )
             else:
-                raise GraphQLError('error: incorrect password')
+                raise GraphQLError("error: incorrect password")
         else:
-            raise GraphQLError('error: incorrect phoneNum')
+            raise GraphQLError("error: incorrect phoneNum")
 
 
 class Mutation(graphene.ObjectType):
     auth = AuthMutation.Field()
     signUp = SignUpMutation.Field()
 
+
 # Queries
 
 
 class Query(graphene.ObjectType):
     node = graphene.relay.Node.Field()
-    person = graphene.Field(Person, uuid=graphene.String(
-        default_value=""), jwt=graphene.String(default_value=""))
+    person = graphene.Field(
+        Person,
+        uuid=graphene.String(default_value=""),
+        jwt=graphene.String(default_value=""),
+    )
+
     def resolve_person(self, info, uuid, jwt):
         auth_uuid = ""
         if jwt != "":
@@ -92,23 +109,33 @@ class Query(graphene.ObjectType):
             return query.get(auth_uuid)
         else:
             return query.get(uuid)
+
     uuid = graphene.String(phoneNum=graphene.String())
+
     def resolve_uuid(self, info, phoneNum):
-        query = db_session.query(PersonModel).filter(
-            PersonModel.phone_num == phoneNum).first()
+        query = (
+            db_session.query(PersonModel)
+            .filter(PersonModel.phone_num == phoneNum)
+            .first()
+        )
         if query:
             return query.uuid
         else:
-            raise GraphQLError('error: no user by that phoneNum')
+            raise GraphQLError("error: no user by that phoneNum")
+
     infectionLog = graphene.List(graphene.String, backLog=graphene.Int())
+
     def resolve_infectionLog(self, info, backLog):
-        paths = sorted(pathlib.Path(os.path.join(UPLOAD_DIR, 'logs')).iterdir(), key=os.path.getmtime)
+        paths = sorted(
+            pathlib.Path(os.path.join(UPLOAD_DIR, "logs")).iterdir(),
+            key=os.path.getmtime,
+        )
         log_paths = []
-        backLog_epoch = time.time()-((24*60*60)*backLog)
+        backLog_epoch = time.time() - ((24 * 60 * 60) * backLog)
         for path in paths:
             if path.stat().st_mtime < backLog_epoch:
                 break
-            log_paths.append(f'{request.url_root}download/logs/{path.name}')
+            log_paths.append(f"{request.url_root}download/logs/{path.name}")
         return log_paths
 
 
