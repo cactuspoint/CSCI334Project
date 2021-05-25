1bd0ef5efe4ea022a053f21d04090180b657fe87
Alex Carter <acarter010204@gmail.com>
2021-05-25 17:09:45 +1000

    Added access level key

--- server/gql_server/models.py
+++ server/gql_server/models.py
@@ -2,15 +2,19 @@ import sqlalchemy as sqa
 from sqlalchemy.orm import scoped_session, sessionmaker, relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = sqa.create_engine('sqlite:///db.sqlite3', convert_unicode=True)
-db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+engine = sqa.create_engine("sqlite:///db.sqlite3", convert_unicode=True)
+db_session = scoped_session(
+    sessionmaker(autocommit=False, autoflush=False, bind=engine)
+)
 
 Base = declarative_base()
 Base.query = db_session.query_property()
 
+
 class Person(Base):
-    __tablename__ = 'Person'
+    __tablename__ = "Person"
     uuid = sqa.Column(sqa.String, primary_key=True)
+    access = sqa.Column(sqa.Integer)
     phone_num = sqa.Column(sqa.String)
     password = sqa.Column(sqa.String)
     first_name = sqa.Column(sqa.String)
