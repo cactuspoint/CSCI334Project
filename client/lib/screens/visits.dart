ae4fe9d7b0cbd90e25018a36606fe2ec470c890d
Alex Carter <acarter010204@gmail.com>
2021-05-27 00:01:01 +1000

    Updated visits page

--- client/lib/screens/visits.dart
+++ client/lib/screens/visits.dart
@@ -2,6 +2,7 @@ import 'package:client/utils/constants/app_globals.dart' as globals;
 import 'package:flutter/material.dart';
 import 'package:client/utils/helpers/database-helper.dart';
 import 'package:client/widgets/qrcode-scan.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class VisitsPage extends StatefulWidget {
   @override
@@ -13,16 +14,27 @@ class _VisitsPageState extends State<VisitsPage> {
 
   @override
   Widget build(BuildContext context) {
+    const String getExposed = '''
+    query GetPerson(\$uuid: String!, \$jwt: String!) {
+      person(uuid: \$uuid, jwt: \$jwt) {
+        infected
+      }
+    }
+    ''';
     return Scaffold(
         body: Padding(
-      padding: const EdgeInsets.only(top: 50),
+      padding: const EdgeInsets.all(20),
       child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Text('Set the location of the visit to be logged:'),
+            SizedBox(height: 10),
             ElevatedButton(
-              child: Text('scan QR code'),
+              child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(16.0),
+                  child: Text('scan QR code')),
               onPressed: () {
                 Navigator.push(
                   context,
@@ -30,27 +42,64 @@ class _VisitsPageState extends State<VisitsPage> {
                 );
               },
             ),
+            SizedBox(height: 20),
             Text('\nPress -Log Visit- to log your visit:'),
+            SizedBox(height: 10),
             ElevatedButton(
-              child: Text('Log Visit'),
+              child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(16.0),
+                  child: Text('Log Visit')),
               onPressed: () {
                 DatabaseHelper.insertVisit(Visit(
                     DateTime.now().toIso8601String(), globals.currentLocation));
               },
             ),
+            SizedBox(height: 20),
             Text('\nPress -Exposed?- to check if you have been exposed:'),
-            ElevatedButton(
-              child: Text('Exposed?'),
-              onPressed: () {
-                checkExposed();
-              },
-            ),
-            Text('Exposure status: ${exposed.toString()}'),
+            SizedBox(height: 10),
+            Query(
+                options: QueryOptions(
+                  document: gql(
+                      getExposed), // this is the query string you just created
+                  variables: {'uuid': "", 'jwt': globals.jwt},
+                  pollInterval: Duration(seconds: 10),
+                ),
+                builder: (QueryResult result,
+                    {VoidCallback refetch, FetchMore fetchMore}) {
+                  var infectionStatus = "Not currently infected";
+                  if (!(result.data['person']['infected'] == "" ||
+                      result.data['person']['infected'] == null)) {
+                    infectionStatus = "Warning : infected";
+                  }
+                  return ElevatedButton(
+                    child: Container(
+                        width: double.infinity,
+                        padding: EdgeInsets.all(16.0),
+                        child: Text(infectionStatus)),
+                    onPressed: () {
+                      refetch();
+                    },
+                  );
+                }),
           ]),
     ));
   }
 
   checkExposed() async {
+    const String getPerson = '''
+    query GetPerson(\$uuid: String!, \$jwt: String!) {
+      person(uuid: \$uuid, jwt: \$jwt) {
+        uuid,
+        phoneNum,
+        firstName, 
+        lastName,
+        pfp,
+        vaccineName,
+        access,
+      }
+    }
+    ''';
     exposed = await DatabaseHelper.exposed();
     setState(() {});
   }
