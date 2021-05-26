6fad00c6bac601743dad1a5b9fecbb6bed3f8e5f
cactuspoint <pollywaffle101@gmail.com>
2021-05-26 15:14:07 +1000

    revert statisitcs-helper to prev version

--- client/lib/utils/helpers/statistics-helper.dart
+++ client/lib/utils/helpers/statistics-helper.dart
@@ -5,37 +5,24 @@ class StatisticsHelper {
   static Future<String> getStatistics() async {
     var covid19Client = Covid19Client();
     var summary = await covid19Client.getSummary();
+    var live = await covid19Client.getLive(country: 'Australia');
     var auTotalConfirmed_ = await covid19Client.getDayOneTotal(
         country: 'Australia', status: 'confirmed');
     var auTotalRecovered_ = await covid19Client.getDayOneTotal(
         country: 'Australia', status: 'recovered');
     var auTotalDeaths_ = await covid19Client.getDayOneTotal(
         country: 'Australia', status: 'deaths');
-    var live = await covid19Client.getLive(country: 'Australia');
-    String auActiveCases = live[live.length - 1].active.toString();
-    String auTotalConfirmed =
-        auTotalConfirmed_[auTotalConfirmed_.length - 1].cases.toString();
-    String auTotalRecovered =
-        auTotalRecovered_[auTotalRecovered_.length - 1].cases.toString();
-    String auTotalDeaths =
-        auTotalDeaths_[auTotalDeaths_.length - 1].cases.toString();
-    String globalNewConfirmed = summary.global.newConfirmed.toString();
-    String globalNewRecovered = summary.global.newRecovered.toString();
-    String globalNewDeaths = summary.global.newDeaths.toString();
-    String globalTotalConfirmed = summary.global.totalConfirmed.toString();
-    String globalTotalRecovered = summary.global.totalRecovered.toString();
-    String globalTotalDeaths = summary.global.totalDeaths.toString();
 
     return Future.value('\n'
-        'Australia Active cases: ${auActiveCases}\n'
-        'Australia Total confirmed: ${auTotalConfirmed}\n'
-        'Australia Total recovered: ${auTotalRecovered}\n'
-        'Australia Total deaths: ${auTotalDeaths}\n'
-        'Global New confirmed: ${globalNewConfirmed}\n'
-        'Global New recovered: ${globalNewRecovered}\n'
-        'Global New deaths: ${globalNewDeaths}\n'
-        'Global Total confirmed: ${globalTotalConfirmed}\n'
-        'Global Total recovered: ${globalTotalRecovered}\n'
-        'Global Total deaths: ${globalTotalDeaths}\n');
+        'Australia Active cases: ${live[live.length - 1].active.toString()}\n'
+        'Australia Total confirmed: ${auTotalConfirmed_[auTotalConfirmed_.length - 1].cases.toString()}\n'
+        'Australia Total recovered: ${auTotalRecovered_[auTotalRecovered_.length - 1].cases.toString()}\n'
+        'Australia Total deaths: ${auTotalDeaths_[auTotalDeaths_.length - 1].cases.toString()}\n'
+        'Global New confirmed: ${summary.global.newConfirmed.toString()}\n'
+        'Global New recovered: ${summary.global.newRecovered.toString()}\n'
+        'Global New deaths: ${summary.global.newDeaths.toString()}\n'
+        'Global Total confirmed: ${summary.global.totalConfirmed.toString()}\n'
+        'Global Total recovered: ${summary.global.totalRecovered.toString()}\n'
+        'Global Total deaths: ${summary.global.totalDeaths.toString()}\n');
   }
 }
