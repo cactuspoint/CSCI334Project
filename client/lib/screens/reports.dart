f9d408a56fa88d662aedf3ce07c9aea3370c8afc
Alex Carter <acarter010204@gmail.com>
2021-05-27 11:37:54 +1000

    generated stats

--- client/lib/screens/reports.dart
+++ client/lib/screens/reports.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:client/utils/helpers/statistics-helper.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'dart:convert';
 
 class ReportsPage extends StatefulWidget {
@@ -10,6 +11,13 @@ class ReportsPage extends StatefulWidget {
 class _ReportsPageState extends State<ReportsPage> {
   @override
   Widget build(BuildContext context) {
+    const String getLogs = '''
+    query getLogs(\$backlog: Int!) {
+      injectionLog(backLog:\$backlog),
+      infectionLog(backLog:\$backlog)
+    }
+    ''';
+    List<String> lines = [];
     return Scaffold(
         body: Center(
             child: FutureBuilder<String>(
@@ -19,25 +27,48 @@ class _ReportsPageState extends State<ReportsPage> {
                     // backing data
                     LineSplitter ls = new LineSplitter();
 
-                    List<String> lines = ls.convert(snapshot.data);
-                    lines.removeAt(0);
-                    return ListView.builder(
-                      itemCount: lines.length,
-                      itemBuilder: (context, index) {
-                        return Card(
-                          //                           <-- Card widget
-                          child: ListTile(
-                            title: Text(lines[index]),
-                          ),
-                        );
-                      },
+                    lines += ls.convert(snapshot.data);
+                    lines.remove("");
+                    return Padding(
+                      padding: EdgeInsets.all(16),
+                      child: ListView.builder(
+                        itemCount: lines.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                              padding: EdgeInsets.only(bottom: 10),
+                              child: Card(
+                                //                           <-- Card widget
+                                child: ListTile(
+                                  title: Text(lines[index]),
+                                ),
+                              ));
+                        },
+                      ),
                     );
                   } else {
                     return Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Text('loading statistics\n'),
-                          CircularProgressIndicator()
+                          CircularProgressIndicator(),
+                          Query(
+                              options: QueryOptions(
+                                document: gql(
+                                    getLogs), // this is the query string you just created
+                                variables: {'backlog': 1},
+                                pollInterval: Duration(seconds: 10),
+                              ),
+                              builder: (QueryResult result,
+                                  {VoidCallback refetch, FetchMore fetchMore}) {
+                                print(result);
+                                lines.add("App detected cases in past day: " +
+                                    result.data['infectionLog'].length
+                                        .toString());
+                                lines.add(
+                                    "App detected injections in past day: " +
+                                        result.data['injectionLog'].toString());
+                                return Text('\nloaded 1/2');
+                              }),
                         ]);
                   }
                 })));
